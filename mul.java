f3f84458ba23504ef5fde0920f318f4b7acfa84c
reshmareddy28 <41780060+reshmareddy28@users.noreply.github.com>
2018-08-01 15:20:21 +0530

    Update mul.java

--- mul.java
+++ mul.java
@@ -1,13 +1,13 @@
 import java.util.Scanner;
 class Ideone
 {
-	public static void main (String[] args) throws java.lang.Exception
+		public static void main (String[] args) throws java.lang.Exception
 	{
-			  Scanner sc = new Scanner(System.in);
+		  Scanner sc = new Scanner(System.in);
         int no = 0;
         no = sc.nextInt();
         for (int i = 0; i <5; i++) {
-            System.out.println(no * (i + 1));
+            System.out.println(no * (i + 1)+"\t");
         }
     }
 }
