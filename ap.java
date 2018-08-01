546e9987e7cd0b9ada644bde627bfa379af66097
reshmareddy28 <41780060+reshmareddy28@users.noreply.github.com>
2018-08-01 15:35:53 +0530

    Update and rename mul.java to ap.java

---
+++ ap.java
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+class Ideone
+{
+		public static void main (String[] args) throws java.lang.Exception
+	{
+		 	 Scanner sc = new Scanner(System.in);
+		 int a=sc.nextInt();
+		 int d=sc.nextInt();
+		 int n=sc.nextInt();
+		System.out.print(n*(2*a+(n-1)*d)/2);
+	}
+}
