bb9960a020256e858bebb38e6fea7a6a8e2d5d2a
reshmareddy28 <41780060+reshmareddy28@users.noreply.github.com>
2018-08-11 12:32:32 +0530

    Update and rename kabali.java to ascii.java

--- ascii.java
+++ ascii.java
@@ -4,9 +4,8 @@ class Ideone
 	public static void main (String[] args) throws java.lang.Exception
 	{
 		Scanner sc=new Scanner(System.in);
-		int a=sc.nextInt();
-		int b=sc.nextInt();
-		int c=Math.abs(a-b);
-		System.out.println(c);
+		char c=sc.next().charAt(0);
+		int a=c;
+		System.out.println(a);
 	}
 }
