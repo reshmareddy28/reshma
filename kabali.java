6087a8b1364e5ceb6d4a4db64d37e7762e6a3160
reshmareddy28 <41780060+reshmareddy28@users.noreply.github.com>
2018-08-11 12:27:40 +0530

    Update and rename gcd.java to kabali.java

--- kabali.java
+++ kabali.java
@@ -6,17 +6,7 @@ class Ideone
 		Scanner sc=new Scanner(System.in);
 		int a=sc.nextInt();
 		int b=sc.nextInt();
-		int c=gcd(a,b);
+		int c=Math.abs(a-b);
 		System.out.println(c);
-	}
-	static int gcd(int a,int b)
-	{
-		if(a==0||b==0)
-			return 0;
-		if(a==b)
-			return a;
-		if(a>b)
-			return gcd(a-b,b);
-		return gcd(a,b-a);
 	}
 }
