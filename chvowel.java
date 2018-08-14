4e873827438be3fffa6ce42a01028e3e98c15c0d
reshmareddy28 <41780060+reshmareddy28@users.noreply.github.com>
2018-08-14 11:49:06 +0530

    Update and rename chkvowel.java to chvowel.java

--- chvowel.java
+++ chvowel.java
@@ -3,21 +3,23 @@ class Ideone
 {
 	public static void main (String[] args) throws java.lang.Exception
 	{
+		int i;
 		Scanner sc=new Scanner(System.in);
 		String str =sc.nextLine();
-		for(int i=0;i <str.length();i++)
+		for(i=0;i <str.length();i++)
 		{
             		if((str.charAt(i) == 'a')||(str.charAt(i) == 'e') ||(str.charAt(i) == 'i')||(str.charAt(i) == 'o')||(str.charAt(i) == 'u')) 
             		{
             			System.out.println("yes");
             			break;
             		}
-            		else
+		}
+            		if(i==str.length())
             		{
             			System.out.println("no");
-            			break;
+            			
             			
             		}
-		}
+		
 	}
 }
