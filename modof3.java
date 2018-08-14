d9dd84179cd7beb47b51ee62a5f5730416277244
reshmareddy28 <41780060+reshmareddy28@users.noreply.github.com>
2018-08-14 12:29:40 +0530

    Create modof3.java

---
+++ modof3.java
@@ -0,0 +1,18 @@
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+/* Name of the class has to be "Main" only if the class is public. */
+class Ideone
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		Scanner sc=new Scanner(System.in);
+		int a=sc.nextInt();
+		int b=sc.nextInt();
+		int c=sc.nextInt();
+		int d;
+		d=(a*b)%c;
+		System.out.print(d);
+	}
+}
