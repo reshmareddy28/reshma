dbe8256620d6b6dbfffed479274ca3ee95f3d814
reshmareddy28 <41780060+reshmareddy28@users.noreply.github.com>
2018-08-14 12:22:03 +0530

    Create simpleintrest.java

---
+++ simpleintrest.java
@@ -0,0 +1,18 @@
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+/* Name of the class has to be "Main" only if the class is public. */
+class Ideone
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		Scanner sc=new Scanner(System.in);
+	    int p=sc.nextInt();
+	    int t=sc.nextInt();
+	    int r=sc.nextInt();
+	    int si;
+	    si=(p*t*r)/100;
+	    System.out.print(si);
+	}
+}
